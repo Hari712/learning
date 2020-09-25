378e58565ff9e8c04589779b98c704ecb637a82a
khushbu-solanki <khushbu.solanki@ekzero.com>
2020-09-25 14:38:59 +0530

    completed TrackingDetails.js screen

--- Gtrack/src/component/TrackingDetails.js
+++ Gtrack/src/component/TrackingDetails.js
@@ -1,14 +1,13 @@
 import React, { useState, useRef } from 'react';
-import { View, StyleSheet, Text, Image, TouchableOpacity, Platform } from 'react-native';
+import { View, StyleSheet, Text, Image, Dimensions, Platform } from 'react-native';
 import images from '../constants/images';
 import { ColorConstant } from '../constants/ColorConstants'
-import { FontSize } from './FontSize'
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
-import BottomSheet from "react-native-raw-bottom-sheet";
 import SlidingUpPanel from 'rn-sliding-up-panel';
 
 
 const isAndroid = Platform.OS === 'android'
+const {height} = Dimensions.get('window')
 
 const Map = Platform.select({
 	ios: () => require('react-native-maps'),
@@ -17,12 +16,12 @@ const Map = Platform.select({
 
 const TrackingDetails = () => {
 
-    //const refRBSheet = useRef();
-    const _panel = useRef();
+	const _panel = useRef();
     
     React.useEffect(()=>{
-        //refRBSheet.current.open()
-       _panel.current.show()
+
+	   _panel.current.show()
+	   
     },[])
 
 	function renderMapBox() {
@@ -32,8 +31,9 @@ const TrackingDetails = () => {
 				<Map.default.MapView style={{ flex: 1 }}>
 					<Map.default.UserLocation
 						renderMode='normal'
-						visible={true}
+						visible={true}						
 						showsUserHeadingIndicator={true}
+						
 					/>
 				</Map.default.MapView>
 			</View>
@@ -66,71 +66,47 @@ const TrackingDetails = () => {
 
             <SlidingUpPanel 
                 ref={_panel}
-                draggableRange={{top:hp(60),bottom:55}}
-                height={hp(60)}
-                //backdropOpacity={2}
-                containerStyle={{backgroundColor:ColorConstant.WHITE,borderRadius:20,borderWidth:1}}
-                >    
+				draggableRange={{top: height / 2, bottom: hp(9)}}				
+                containerStyle={styles.containerStyle}
+                > 
 
-            {/* <BottomSheet
-                ref={refRBSheet}
-                closeOnDragDown={true}
-                closeOnPressMask={false}
-                minClosingHeight={20}
-                height={hp(50)}
-                keyboardAvoidingViewEnabled={true}        
-                customStyles={{
-                    wrapper: {
-                        backgroundColor: "transparent",
-                        // width:'98%',
-                        // borderColor:'red',
-                        // borderWidth:1,
-                        // alignSelf:'center'
-                
-                    },                    
-                    // draggableIcon: {
-                    //     backgroundColor: ColorConstant.ORANGE
-                    // }
-                }}> */}
-
-                <View style={{width:'100%',alignItems:'center',marginTop:hp(2)}}>
-                    <Text>Tracking Details</Text>
-                    <View style={{flexDirection:'row',marginHorizontal:hp(4),marginTop:hp(4) ,alignItems:'center',borderBottomColor:ColorConstant.GREY,borderBottomWidth:0.5}}>
-                        <Text style={{flex:1,color:ColorConstant.GREY,fontSize:hp(1.5),fontWeight:'bold',marginBottom:hp(1)}}>Address</Text>
+                <View style={styles.subView}>
+                    <Text style={styles.mainTitle}>Tracking Details</Text>
+                    <View style={styles.subContainerView}>
+                        <Text style={styles.title}>Address</Text>
                         <Image style={{marginBottom:hp(1)}} source={images.dashBoard.pin}/>
                     </View> 
-                    <View style={{width:'20%',alignSelf:'flex-start',marginHorizontal:hp(4),marginTop:hp(1)}}>
+                    <View style={styles.address}>
                         <Text style={{fontSize:hp(1)}}>900 Dufferian Street,Toronto MG L40 1V6 Canada</Text>
                     </View>
-                    <View style={{flexDirection:'row',marginHorizontal:hp(4),marginTop:hp(4) ,alignItems:'center',borderBottomColor:ColorConstant.GREY,borderBottomWidth:0.5}}>
-                        <Text style={{flex:1,color:ColorConstant.GREY,fontSize:hp(1.5),fontWeight:'bold',marginBottom:hp(1)}}>Date & Time</Text>
+                    <View style={styles.subContainerView}>
+                        <Text style={styles.title}>Date & Time</Text>
                         <Image style={{marginBottom:hp(1)}} source={images.dashBoard.calender}/>
                     </View> 
-                    <View style={{alignSelf:'flex-start',marginHorizontal:hp(4),marginTop:hp(1),flexDirection:'row'}}>
-                        <Text style={{fontSize:hp(1),flex:0.8}}>21/07/2020</Text>
-                        <Text style={{fontSize:hp(1),flex:1}}>02:00:04</Text>
+                    <View style={styles.otherDetails}>
+                        <Text style={styles.date}>21/07/2020</Text>
+                        <Text style={[styles.date,{flex:1}]}>02:00:04</Text>
                     </View> 
-                    <View style={{flexDirection:'row',marginHorizontal:hp(4),marginTop:hp(4) ,alignItems:'center',borderBottomColor:ColorConstant.GREY,borderBottomWidth:0.5}}>
-                        <Text style={{flex:1,color:ColorConstant.GREY,fontSize:hp(1.5),fontWeight:'bold',marginBottom:hp(1)}}>Date & Time</Text>
+                    <View style={styles.subContainerView}>
+                        <Text style={styles.title}>Other details</Text>
                         <Image style={{marginBottom:hp(1)}} source={images.dashBoard.list}/>
                     </View> 
-                    <View style={{alignSelf:'flex-start',marginHorizontal:hp(4),marginTop:hp(1),flexDirection:'row'}}>
+                    <View style={styles.otherDetails}>
                         <View style={{flex:1}}>
-                            <Text style={{color:ColorConstant.GREY,fontSize:hp(1.2),marginBottom:hp(1)}}>Duration</Text>
-                            <Text style={{color:ColorConstant.BLACK,fontSize:hp(1.2),marginBottom:hp(1)}}>12m 8s</Text>
+                            <Text style={styles.otherDetailText}>Duration</Text>
+                            <Text style={[styles.otherDetailText,{color:ColorConstant.BLACK}]}>12m 8s</Text>
                         </View>
                         <View style={{flex:1}}>
-                            <Text style={{color:ColorConstant.GREY,fontSize:hp(1.2),marginBottom:hp(1)}}>Distance</Text>
-                            <Text style={{color:ColorConstant.BLACK,fontSize:hp(1.2),marginBottom:hp(1)}}>16.47mi</Text>
+                            <Text style={styles.otherDetailText}>Distance</Text>
+                            <Text style={[styles.otherDetailText,{color:ColorConstant.BLACK}]}>16.47mi</Text>
                         </View>
                         <View style={{flex:0.3}}>
-                            <Text style={{color:ColorConstant.GREY,fontSize:hp(1.2),marginBottom:hp(1)}}>Speed</Text>
-                            <Text style={{color:ColorConstant.BLACK,fontSize:hp(1.2),marginBottom:hp(1)}}>66mph</Text>
+                            <Text style={styles.otherDetailText}>Speed</Text>
+                            <Text style={[styles.otherDetailText,{color:ColorConstant.BLACK}]}>66mph</Text>
                         </View>    
                     </View>         
                 </View>
                 </SlidingUpPanel>
-            {/* </BottomSheet> */}
 
 			{/* <Mapbox.MapView
 				styleURL={Mapbox.StyleURL.Street}
@@ -208,6 +184,55 @@ const styles = StyleSheet.create({
 		borderRadius: 15,
 		backgroundColor: 'blue',
 		transform: [{ scale: 0.6 }]
+	},
+	containerStyle: {
+		backgroundColor:ColorConstant.WHITE,
+		borderTopLeftRadius:30,
+		borderTopRightRadius:30
+	},
+	subView: {
+		width:'100%',
+		alignItems:'center',
+		marginTop:hp(2)
+	},
+	mainTitle: {
+		color:ColorConstant.ORANGE,
+		fontWeight:'bold',
+		fontSize:hp(2)
+	},
+	subContainerView: {
+		flexDirection:'row',
+		marginHorizontal:hp(4),
+		marginTop:hp(4) ,
+		alignItems:'center',
+		borderBottomColor:ColorConstant.GREY,
+		borderBottomWidth:0.5
+	},
+	title: {
+		flex:1,color:ColorConstant.GREY,
+		fontSize:hp(1.5),
+		fontWeight:'bold',
+		marginBottom:hp(1)
+	},
+	address: {
+		width:'20%',
+		alignSelf:'flex-start',
+		marginHorizontal:hp(4),
+		marginTop:hp(1)
+	},
+	date: {
+		fontSize:hp(1),
+		flex:0.8
+	},
+	otherDetails: {
+		alignSelf:'flex-start',
+		marginHorizontal:hp(4),
+		marginTop:hp(1),
+		flexDirection:'row'
+	},
+	otherDetailText: {
+		color:ColorConstant.GREY,fontSize:hp(1.2),
+		marginBottom:hp(1)
 	}
 });
 
