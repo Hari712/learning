eb69710c01af511866f14b25f893d00a9ed57d51
khushbu-solanki <khushbu.solanki@ekzero.com>
2020-10-15 11:11:54 +0530

    validation

--- Gtrack/src/utils/helper.js
+++ Gtrack/src/utils/helper.js
@@ -1,6 +1,6 @@
-import { EMAIL_PHONE_REGEX } from '../constants/AppConstants'
+import { EMAIL_VALIDATION_REGEX } from '../constants/AppConstants'
 
 export const validateEmailorPhoneNumber = (input) => {
-    const re = EMAIL_PHONE_REGEX
+    const re = EMAIL_VALIDATION_REGEX
     return re.test(input)
 }
