4396ddd08292e0f33b1ec5730f3b1625923316e3
kinnari kansara <kinnari.kansara@ekzero.com>
2020-11-05 14:26:06 +0530

    [VOGT-137] - DESIGN CHANGES IN NOTIFICATION

--- Gtrack/src/screen/Notification/Notification.js
+++ Gtrack/src/screen/Notification/Notification.js
@@ -1,24 +1,18 @@
-import React, {useState, Component} from 'react';
+import React, { useState, Component } from 'react';
 import { View, Text, TouchableOpacity, Image, StyleSheet, SafeAreaView } from 'react-native';
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
 import images from '../../constants/images';
 import { ColorConstant } from '../../constants/ColorConstants';
 import FontSize from '../../component/FontSize';
 import { FlatList } from 'react-native-gesture-handler';
+import Tooltip from 'rn-tooltip';
 
 const Notification = ({ navigation }) => {
 
     React.useLayoutEffect(() => {
         navigation.setOptions({
             headerTitle: () => (
-                <Text style={{
-                    color: ColorConstant.GREY,
-                    fontSize: FontSize.FontSize.medium,
-                    fontWeight: '500',
-                    textAlign: 'center'
-                }}>
-                    Notifications
-                </Text>
+                <Text style={styles.headingTitle}> Notifications </Text>
             ),
             headerLeft: () => (
                 <TouchableOpacity onPress={() => navigation.goBack()}>
@@ -28,29 +22,44 @@ const Notification = ({ navigation }) => {
         });
     }, [navigation]);
 
-    const NotificationItems = ({item}) => {
-        return(
+    const NotificationItems = ({ item }) => {
+        return (
             <View>
-                <View style={{ flexDirection: 'row', flex: 1, paddingVertical: hp(1.5), paddingHorizontal: wp(1.5) }}>
-                    <View style={{ flexDirection: 'row', flex: 0.8, alignItems: 'center' }}>
-                        <View style={{ flex: 0.25 }}>
+                <View style={styles.notificationMainView}>
+                    <View style={styles.notificationLeftMainView}>
+                        <View style={styles.notificationLeftView}>
                             <View style={styles.notificationDetailView}>
-                                <Image source={item.titleIcon} style={{ height: hp(3.5), width: hp(3.5) }} resizeMode='contain' />
+                                <Image source={item.titleIcon} style={styles.imageStyle} resizeMode='contain' />
                             </View>
                         </View>
 
-                        <View style={{ paddingHorizontal: '3%', marginTop: hp(1), flex: 0.75 }}>
-                            <Text style={{ color: ColorConstant.BLACK, fontSize: FontSize.FontSize.small, fontWeight: 'bold' }}>{item.title}</Text>
-                            <Text style={{ color: ColorConstant.GREY, fontSize: hp(1.4), marginTop: hp(1) }}>{item.device}</Text>
-                            <Text style={{ color: ColorConstant.GREY, fontSize: hp(1.2), marginTop: hp(1) }}>{item.description}</Text>
-                            <Text style={{ color: ColorConstant.GREY, fontSize: hp(1.2), marginTop: hp(1) }}>{item.speed}</Text>
+                        <View style={styles.notificationRightView}>
+                            <Text style={styles.titleStyle}>{item.title}</Text>
+                            <View style={{ flexDirection: 'row' }}>
+                                <Text style={styles.deviceStyle}>{item.device}</Text>
+                                <Tooltip popover={
+                                    <View>
+                                        <Text>UK Van</Text>
+                                    </View>
+
+                                }
+                                    backgroundColor={ColorConstant.WHITE}
+                                    overlayColor={ColorConstant.TRANSPARENT}
+                                    pointerStyle={{ elevation: 0.1, borderRightWidth: 4, borderLeftWidth: 4 }}
+                                    containerStyle={{ borderColor: ColorConstant.ORANGE, borderWidth: 1, borderRadius: 6 }}
+                                >
+                                    <Text style={[styles.deviceStyle, { marginLeft: wp(4) }]}>+2</Text>
+                                </Tooltip>
+                            </View>
+                            <Text style={styles.descriptionStyle}>{item.description}</Text>
+                            <Text style={styles.speedTextStyle}>{item.speed}</Text>
                         </View>
                     </View>
 
-                    <View style={{ flex: 0.2, justifyContent: 'center', alignItems: 'flex-end', }}>
+                    <View style={styles.notificationRightMainView}>
                         <View style={styles.stateViewStyle}>
-                            <Text style={{ color: ColorConstant.GREY, fontSize: hp(1.2) }}>{item.time}</Text>
-                            <Image source={item.crossIcon} style={{ height: hp(1.3), width: hp(1.3) }} resizeMode='contain' />
+                            <Text style={styles.timeTextStyle}>{item.time}</Text>
+                            <Image source={item.crossIcon} style={styles.crossImageStyle} resizeMode='contain' />
                         </View>
                     </View>
                 </View>
@@ -60,31 +69,85 @@ const Notification = ({ navigation }) => {
         )
     }
 
-    return(
+    return (
         <SafeAreaView style={styles.container}>
             <FlatList
                 styles={{}}
                 contentContainerStyle={{}}
                 data={DATA}
                 renderItem={NotificationItems}
-                keyExtractor={(item,index) => index.toString()}
+                keyExtractor={(item, index) => index.toString()}
             />
         </SafeAreaView>
     )
 }
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
+    headingTitle: {
+        color: ColorConstant.GREY,
+        fontSize: FontSize.FontSize.medium,
+        fontWeight: '500',
+        textAlign: 'center'
+    },
     container: {
         flex: 1,
         backgroundColor: ColorConstant.WHITE,
+    },
+    notificationMainView: {
+        flexDirection: 'row',
+        flex: 1,
+        paddingVertical: hp(1.5),
+        paddingHorizontal: wp(1.5)
+    },
+    notificationLeftMainView: {
+        flexDirection: 'row',
+        flex: 0.8,
+        alignItems: 'center'
+    },
+    notificationRightMainView: {
+        flex: 0.2,
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+    },
+    notificationLeftView: {
+        flex: 0.25
     },
     notificationDetailView: {
         width: hp(6),
         height: hp(6),
         justifyContent: 'center',
         alignItems: 'center'
-      },
-      stateViewStyle: {
+    },
+    imageStyle: {
+        height: hp(3.5),
+        width: hp(3.5)
+    },
+    notificationRightView: {
+        paddingHorizontal: '3%',
+        marginTop: hp(1),
+        flex: 0.75
+    },
+    titleStyle: {
+        color: ColorConstant.BLACK,
+        fontSize: FontSize.FontSize.small,
+        fontWeight: 'bold'
+    },
+    deviceStyle: {
+        color: ColorConstant.GREY,
+        fontSize: hp(1.4),
+        marginTop: hp(1)
+    },
+    descriptionStyle: {
+        color: ColorConstant.GREY,
+        fontSize: hp(1.2),
+        marginTop: hp(1)
+    },
+    speedTextStyle: {
+        color: ColorConstant.GREY,
+        fontSize: hp(1.2),
+        marginTop: hp(1)
+    },
+    stateViewStyle: {
         width: '90%',
         height: hp(2.5),
         justifyContent: 'center',
@@ -93,11 +156,19 @@ const styles = StyleSheet.create ({
         flexDirection: 'row',
         justifyContent: 'space-between',
         paddingRight: wp(2)
-      },
-      lineStyle: {
-        borderBottomColor: ColorConstant.GREY, 
-        borderBottomWidth: 0.3 
-      },
+    },
+    timeTextStyle: {
+        color: ColorConstant.GREY,
+        fontSize: hp(1.2)
+    },
+    crossImageStyle: {
+        height: hp(1.3),
+        width: hp(1.3)
+    },
+    lineStyle: {
+        borderBottomColor: ColorConstant.GREY,
+        borderBottomWidth: 0.5
+    },
 })
 
 export default Notification;
@@ -105,7 +176,7 @@ export default Notification;
 const DATA = [
     {
         titleIcon: images.notification.speed,
-        title: 'Speed',
+        title: 'Over Speed',
         device: 'TrackPort 4G Vehical GPS Tracker',
         description: 'Speed Violation Limit 50mph',
         speed: 'Speed 75Mph',
@@ -114,18 +185,10 @@ const DATA = [
     },
     {
         titleIcon: images.notification.battery,
-        title: 'Battery',
+        title: 'Battery Level',
         device: 'Spark Nano 7 GPS Tracker',
         description: 'Low Battery 20%',
         time: '10h ago',
-        crossIcon: images.notification.crossIcon
-    },
-    {
-        titleIcon: images.notification.car,
-        title: 'Tow',
-        device: 'Spark Nano 7 GPS Tracker',
-        description: 'Your vehicle is being towed.',
-        time: '1d ago',
         crossIcon: images.notification.crossIcon
     },
     {
@@ -138,7 +201,7 @@ const DATA = [
     },
     {
         titleIcon: images.notification.engineStatus,
-        title: 'Engine Status',
+        title: 'Engine Idle',
         device: 'UK Van',
         description: 'Your vehicle engine is die',
         time: '1d ago',
