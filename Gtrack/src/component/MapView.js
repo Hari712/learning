ce2053ad9a5019ae6887957aa7c380c3cf1f3aa0
Khushbu Solanki <khushbu.solanki@ekzero.com>
2020-11-10 17:52:48 +0530

    Added polygon and tested markerView

--- Gtrack/src/component/MapView.js
+++ Gtrack/src/component/MapView.js
@@ -5,9 +5,13 @@ import { ColorConstant } from '../constants/ColorConstants'
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
 import BottomSheet from 'reanimated-bottom-sheet';
 import FontSize from './FontSize';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const isAndroid = Platform.OS === 'android'
 const {height} = Dimensions.get('window')
+
+var points1 = [-7.941227, 39.584127]
+
 
 const Map = Platform.select({
 	ios: () => require('react-native-maps'),
@@ -16,22 +20,67 @@ const Map = Platform.select({
 
 const MapView = () => {
     
-    React.useEffect(()=>{
-
-	   
-    },[])
+    React.useEffect(()=>{},[])
 
 	function renderMapBox() {
-		console.log("android")
+		console.log("Android")
 		return (
 			<View style={{ flex: 1 }}>
-				<Map.default.MapView style={{ flex: 1 }}>
+				<Map.default.MapView style={{ flex: 1 }} >					
+					
 					<Map.default.UserLocation
 						renderMode='normal'
-						visible={true}						
+						visible={true}					
 						showsUserHeadingIndicator={true}
-						
+					
 					/>
+
+					<Map.default.MarkerView 
+						coordinate={points1} 
+						id='point1'
+						draggable = {true}
+						anchor={{x: 0.5, y: 0.5}}
+						onPress={()=>console.log("Something 2")}
+						onDragEnd = {()=>console.log("Something ")}
+						>
+							<View
+								style={{
+								width: 20,
+								height: 20,
+								borderRadius: 10,
+								backgroundColor: ColorConstant.BLACK,
+								}}
+							/>
+					</Map.default.MarkerView>
+
+					<Map.default.Camera
+						centerCoordinate={[-7.946227, 39.589127]}
+						zoomLevel={12}
+					/>
+
+					<Map.default.ShapeSource
+						id="source"
+						onPress={()=>console.log("khushi")}
+						shape={{
+							type: 'Feature',
+							geometry: {
+								type: 'Polygon',
+								coordinates: [
+									[
+										points1,
+										[-7.951227, 39.584127],
+										[-7.965227, 39.589127],
+										[-7.961227, 39.599127],
+										[-7.941227, 39.604127],
+									],
+								],
+							},
+						}}>
+						<Map.default.FillLayer id="fill" style={{fillColor: '#0000ff40'}} />
+						<Map.default.LineLayer id="line" style={{lineColor: '#f008', lineWidth: 4}} />
+						<Map.default.CircleLayer id="circle" style={{circleRadius: 20, circleColor: 'yellow', circleOpacity:0.4, circleStrokeWidth: 2, circleTranslate:[0,0], circleTranslateAnchor:'viewport'}} />
+					</Map.default.ShapeSource>					
+					
 				</Map.default.MapView>
 			</View>
 		)
@@ -46,7 +95,8 @@ const MapView = () => {
                     <Map.default style={StyleSheet.absoluteFillObject} showsUserLocation={true}>
                         {/* {<Map.Marker
                             coordinate={coordinate}
-                            title={selectedRoute.shipmentName}
+							title={selectedRoute.shipmentName}
+
                         />} */}
                     </Map.default>
                     <View style={{ position: 'absolute', top: 100, left: 50 }} />
@@ -60,6 +110,7 @@ const MapView = () => {
 	
 	return (
 		<View style={styles.container}>
+			{console.log("Platform: ",Platform.OS)}
 			{isAndroid ? renderMapBox() : renderApppleMap()}
 
 			{/* <Mapbox.MapView
