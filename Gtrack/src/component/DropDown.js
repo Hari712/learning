1b48df3109e231c1ffbeaf1ebe9de4798be9196c
Rahul Soni <rahul.soni@ekzero.com>
2020-11-19 11:46:34 +0530

    - Wrapped components in AddUser.js with ShadoView

--- Gtrack/src/component/DropDown.js
+++ Gtrack/src/component/DropDown.js
@@ -1,10 +1,11 @@
-import React, {Component, useState } from "react";
+import React, { Component, useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity, Image, SafeAreaView, Modal, FlatList, Dimensions } from "react-native";
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
 import images from '../constants/images'
 import { ColorConstant } from '../constants/ColorConstants'
 import { OutlinedTextField } from 'react-native-material-textfield'
 import FontSize from './FontSize';
+import ShadowView from 'react-native-simple-shadow-view'
 
 
 
@@ -12,9 +13,9 @@ class DropDown extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             isSelected: false,
-            selected:'',
+            selected: '',
             buttonMeasurement: ''
         }
         this._buttonFrame = null;
@@ -22,127 +23,129 @@ class DropDown extends React.Component {
     }
 
     onSubmit = () => {
-        let {current: field } = this.fieldRef;  
+        let { current: field } = this.fieldRef;
     }
 
     render() {
-    
-        //isRelative will set the dropdown view absolute or relative ie over other components or expanding other component
-        const {label, dataList, innerRef, outerStyle, dropdownStyle, isRelative, ...otherProps} = this.props;
 
-        const data = ['Car','Truck','Tempo'];        
+        //isRelative will set the dropdown view absolute or relative ie over other components or expanding other component
+        const { label, dataList, innerRef, outerStyle, dropdownStyle, isRelative, ...otherProps } = this.props;
+
+        const data = ['Car', 'Truck', 'Tempo'];
 
         function handleRightAccessory() {
             return <View style={styles.imageContainer}>
-                    <Image source={images.image.next} resizemode='contain' style={styles.downArrow} />
-                </View>
+                <Image source={images.image.next} resizemode='contain' style={styles.downArrow} />
+            </View>
         }
 
         const show = () => {
-            this.setState({isSelected: !this.state.isSelected})
+            this.setState({ isSelected: !this.state.isSelected })
         }
 
-        return(
+        return (
             <>
-                <TouchableOpacity onPress={show} style={[styles.container, outerStyle]}  
-                    onLayout={({nativeEvent}) => {
-                        console.log("Sub container ",nativeEvent.layout)
-                        this.setState({buttonMeasurement:nativeEvent.layout})
-                        //setSubContainerHeight(nativeEvent.layout.height)
-                    }}>
-                    <OutlinedTextField
-                        label={label}
-                        textColor={ColorConstant.BLACK}
-                        tintColor={ColorConstant.GREY}
-                        baseColor={ColorConstant.GREY}
-                        fontSize={FontSize.FontSize.small}
-                        //labelTextStyle={{ fontFamily: 'Nunito-ExtraLightItalic' }}
-                        labelFontSize={FontSize.FontSize.small}
-                        contentInset={{ input: 10.45, label: 3 }}
-                        //defaultValue={this.state.selected}
-                        renderRightAccessory={() => handleRightAccessory()}
-                        editable={false}
-                        inputContainerStyle={styles.inputContainer}
-                        activeLineWidth={1}
-                        containerStyle={styles.inputButton}
-                        {...otherProps}
-                    />                   
-                </TouchableOpacity>
+                <ShadowView style={styles.shadowContainer}>
+                    <TouchableOpacity onPress={show} style={[styles.container, outerStyle]}
+                        onLayout={({ nativeEvent }) => {
+                            console.log("Sub container ", nativeEvent.layout)
+                            this.setState({ buttonMeasurement: nativeEvent.layout })
+                            //setSubContainerHeight(nativeEvent.layout.height)
+                        }}>
+                        <OutlinedTextField
+                            label={label}
+                            textColor={ColorConstant.BLACK}
+                            tintColor={ColorConstant.GREY}
+                            baseColor={ColorConstant.GREY}
+                            fontSize={FontSize.FontSize.small}
+                            //labelTextStyle={{ fontFamily: 'Nunito-ExtraLightItalic' }}
+                            labelFontSize={FontSize.FontSize.small}
+                            contentInset={{ input: 10.45, label: 3 }}
+                            //defaultValue={this.state.selected}
+                            renderRightAccessory={() => handleRightAccessory()}
+                            editable={false}
+                            inputContainerStyle={styles.inputContainer}
+                            activeLineWidth={1}
+                            containerStyle={styles.inputButton}
+                            {...otherProps}
+                        />
+                    </TouchableOpacity>
+                </ShadowView>
                 {/* </View> */}
 
 
                 { this.state.isSelected ?
-                    <View style={[isRelative ? 
-                        styles.relativeDropdown: 
-                        [styles.absoluteDropdown,{top:this.state.buttonMeasurement.y + this.state.buttonMeasurement.height}]
-                    , dropdownStyle]}>
-                        {(dataList?dataList:data).map((item,key)=>{
-                            return(
-                            <TouchableOpacity style={otherProps.dataRowStyle} key={key} 
-                            onPress={()=>{
-                                this.setState({
-                                    selected: item, 
-                                    isSelected:false,
-                                })
-                                this.props.valueSet(item)
-                            }}>
-                                <Text style={otherProps.dataTextStyle}>{item}</Text>
-                                {key < dataList.length-1 ? 
-                                    <View style={styles.horizontalLine}/>
-                                :null}
-                                
-                            </TouchableOpacity>
+                    <View style={[isRelative ?
+                        styles.relativeDropdown :
+                        [styles.absoluteDropdown, { top: this.state.buttonMeasurement.y + this.state.buttonMeasurement.height }]
+                        , dropdownStyle]}>
+                        {(dataList ? dataList : data).map((item, key) => {
+                            return (
+                                <TouchableOpacity style={otherProps.dataRowStyle} key={key}
+                                    onPress={() => {
+                                        this.setState({
+                                            selected: item,
+                                            isSelected: false,
+                                        })
+                                        this.props.valueSet(item)
+                                    }}>
+                                    <Text style={otherProps.dataTextStyle}>{item}</Text>
+                                    {key < dataList.length - 1 ?
+                                        <View style={styles.horizontalLine} />
+                                        : null}
+
+                                </TouchableOpacity>
                             )
                         })}
                     </View>
-                :null} 
+                    : null}
 
-            </>   
+            </>
         )
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        marginVertical:hp(1),
-        justifyContent: 'center', 
+        marginVertical: hp(1),
+        justifyContent: 'center',
         //backgroundColor:'red'
     },
     downArrow: {
-        marginVertical:hp(1),
+        marginVertical: hp(1),
     },
     horizontalLine: {
-        borderWidth:0.5, 
-        borderRadius:1, 
-        borderColor:ColorConstant.GREY
+        borderWidth: 0.5,
+        borderRadius: 1,
+        borderColor: ColorConstant.GREY
     },
-    relativeDropdown: {       
-        marginTop:hp(0.5), 
-        borderRadius:hp(2),
-        opacity:1, 
-        marginHorizontal:wp(10),
-        alignSelf:'center',  
-        elevation:5, 
-        backgroundColor:'white', 
-        width:'100%',
-        paddingHorizontal:hp(3),
-        paddingLeft:hp(3),
+    relativeDropdown: {
+        marginTop: hp(0.5),
+        borderRadius: hp(2),
+        opacity: 1,
+        marginHorizontal: wp(10),
+        alignSelf: 'center',
+        elevation: 5,
+        backgroundColor: 'white',
+        width: '100%',
+        paddingHorizontal: hp(3),
+        paddingLeft: hp(3),
         borderWidth: 1,
         borderColor: ColorConstant.GREY,
     },
-    absoluteDropdown: { 
-        position:'absolute',        
-        marginTop:hp(0.5), 
-        borderRadius:hp(2),
-        opacity:1, 
-        marginHorizontal:wp(10),
-        alignSelf:'center',  
-        elevation:5, 
-        zIndex:5,
-        backgroundColor:'white', 
-        width:'100%',
-        paddingHorizontal:hp(3),
-        paddingLeft:hp(3),
+    absoluteDropdown: {
+        position: 'absolute',
+        marginTop: hp(0.5),
+        borderRadius: hp(2),
+        opacity: 1,
+        marginHorizontal: wp(10),
+        alignSelf: 'center',
+        elevation: 5,
+        zIndex: 5,
+        backgroundColor: 'white',
+        width: '100%',
+        paddingHorizontal: hp(3),
+        paddingLeft: hp(3),
         borderWidth: 1,
         borderColor: ColorConstant.GREY,
     },
@@ -155,8 +158,18 @@ const styles = StyleSheet.create({
         width: '100%',
         height: hp(6),
     },
+    shadowContainer: {
+        width: '100%',
+        shadowColor: ColorConstant.GREY,
+        shadowOffset: {
+            width: 0,
+            height: 3
+        },
+        shadowRadius: 3,
+        shadowOpacity: 0.3
+    },
 })
-    
+
 
 export default React.forwardRef((props, ref) =>
     <DropDown innerRef={ref} {...props} />
--- Gtrack/src/screen/Users/AddUser.js
+++ Gtrack/src/screen/Users/AddUser.js
@@ -1,5 +1,5 @@
-import React, { useState ,Component} from 'react';
-import { View, StyleSheet,Text, Image,TouchableOpacity, Dimensions, ScrollView, TextInput} from 'react-native';
+import React, { useState, Component } from 'react';
+import { View, StyleSheet, Text, Image, TouchableOpacity, Dimensions, ScrollView, TextInput } from 'react-native';
 import images from '../../constants/images';
 import { ColorConstant } from '../../constants/ColorConstants'
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
@@ -14,8 +14,9 @@ import { getLoginState, getSubuserState } from '../Selector'
 import { useDispatch, useSelector } from 'react-redux';
 import isEmpty from 'lodash/isEmpty'
 import AppManager from '../../constants/AppManager'
+import ShadowView from 'react-native-simple-shadow-view'
 
-const AddUser = ({navigation,route}) => {
+const AddUser = ({ navigation, route }) => {
 
   const { loginData, subUserData } = useSelector(state => ({
     loginData: getLoginState(state),
@@ -25,7 +26,7 @@ const AddUser = ({navigation,route}) => {
 
   const dispatch = useDispatch()
 
-  const [firstName, setFirstName]= useState();
+  const [firstName, setFirstName] = useState();
   const [lastName, setLastName] = useState();
   const [email, setEmail] = useState();
   const [infoClick, setInfoClick] = useState(false);
@@ -34,94 +35,96 @@ const AddUser = ({navigation,route}) => {
 
   React.useEffect(() => {
     dispatch(UsersActions.requestGetGroup(loginData.id, onGroupSuccess, onGroupError));
-    if(route){
+    if (route) {
       const editData = route.params;
-      if(editData){
+      if (editData) {
         setFirstName(editData.editData.firstName)
         setLastName(editData.editData.lastName)
         setEmail(editData.editData.email)
-        setRole(editData.editData.roles[0].id==1?"Owner":"Regular")
+        setRole(editData.editData.roles[0].id == 1 ? "Owner" : "Regular")
         setSelectedGroup(editData.editData.groups)
-    }}
-  },[])
+      }
+    }
+  }, [])
 
-  function onGroupSuccess(data){
-    console.log("Success group",data)
+  function onGroupSuccess(data) {
+    console.log("Success group", data)
   }
 
-  function onGroupError(error){
-    console.log("Error group",error)
+  function onGroupError(error) {
+    console.log("Error group", error)
   }
 
   function addUser() {
-        AppManager.showLoader()
-        if(route && route.params){
-          const requestBody = {
-            "id" : route.params.editData.id,
-            "firstName" : firstName ,
-            "lastName" : lastName,
-            "email" : email,
-            "roles" : [ {
-              "id" : role=="Owner" ? 1 : 2
-            } ],
-            "groups" : selectedGroup
-          }
-          dispatch(UsersActions.requestUpdateSubuserDetail(requestBody, loginData.id, onSuccess, onError)) 
-        } else {
-          const requestBody = {
-            "userDTOS" : [ {
-              "id" : null,
-              "email" : email,
-              "firstName" :firstName,
-              "lastName" : lastName,
-              "markAsOwner" : null,
-              "roles" : [{
-                "id" : role=="Owner" ? 1 : 2
-              }],
-              "groups" : selectedGroup
-            } ]
-          }
-          dispatch(UsersActions.requestAddSubuser(requestBody, loginData.id, onSuccess, onError)) 
-        }
-
+    AppManager.showLoader()
+    if (route && route.params) {
+      const requestBody = {
+        "id": route.params.editData.id,
+        "firstName": firstName,
+        "lastName": lastName,
+        "email": email,
+        "roles": [{
+          "id": role == "Owner" ? 1 : 2
+        }],
+        "groups": selectedGroup
+      }
+      dispatch(UsersActions.requestUpdateSubuserDetail(requestBody, loginData.id, onSuccess, onError))
+    } else {
+      const requestBody = {
+        "userDTOS": [{
+          "id": null,
+          "email": email,
+          "firstName": firstName,
+          "lastName": lastName,
+          "markAsOwner": null,
+          "roles": [{
+            "id": role == "Owner" ? 1 : 2
+          }],
+          "groups": selectedGroup
+        }]
+      }
+      dispatch(UsersActions.requestAddSubuser(requestBody, loginData.id, onSuccess, onError))
     }
 
+  }
 
-function onSuccess(data) {    
-  console.log("Success",data)
-  AppManager.hideLoader()
-}
 
-function onError(error) {
-  AppManager.hideLoader()
-  console.log("Error",error)   
-}
+  function onSuccess(data) {
+    console.log("Success", data)
+    AppManager.hideLoader()
+  }
+
+  function onError(error) {
+    AppManager.hideLoader()
+    console.log("Error", error)
+  }
 
   React.useLayoutEffect(() => {
 
     navigation.setOptions({
-        headerTitle: () => (
-            <Text style={{
-                color:ColorConstant.GREY,
-                fontSize: FontSize.FontSize.medium,
-                fontWeight: '500',
-                //letterSpacing: 0,
-                textAlign:'center' }}>
-                Users
-            </Text>          
-        ),  
-        headerLeft:() => (
-            <TouchableOpacity onPress={()=>navigation.goBack()}>
-            <Image style={{marginLeft:hp(2)}} source={images.image.back}/>
-            </TouchableOpacity>
-        )  
+      headerTitle: () => (
+        <Text style={{
+          color: ColorConstant.GREY,
+          fontSize: FontSize.FontSize.medium,
+          fontWeight: '500',
+          //letterSpacing: 0,
+          textAlign: 'center'
+        }}>
+          Users
+        </Text>
+      ),
+      headerLeft: () => (
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Image style={{ marginLeft: hp(2) }} source={images.image.back} />
+        </TouchableOpacity>
+      )
     });
-  },[navigation]);
+  }, [navigation]);
 
 
 
   const info = () => {
-    return(
+    return (
       <View style={styles.infoContainer}>
         <Text style={styles.infoTitle}>As per the selected role, access rights will be assign as :</Text>
         <View style={styles.infoSubContainer}>
@@ -136,188 +139,196 @@ function onError(error) {
     )
   }
 
-return ( 
+  return (
     <View style={styles.container}>
-      <View style={{width:'100%'}}>
+      <View style={{ width: '100%' }}>
         <TouchableOpacity style={styles.addButton}>
-            <Text style={{fontFamily:'Nunito-Bold',fontSize:16,color:ColorConstant.WHITE}}>{route.params?'Edit User': 'Add User'}</Text>
+          <Text style={{ fontFamily: 'Nunito-Bold', fontSize: 16, color: ColorConstant.WHITE }}>{route.params ? 'Edit User' : 'Add User'}</Text>
         </TouchableOpacity>
       </View>
-      <ScrollView style={{width:'100%'}}>
+      <ScrollView style={{ width: '100%' }}>
         <View style={styles.subContainer}>
-          <TextField 
-            label='First Name*' 
-            valueSet={setFirstName} 
-            defaultValue={firstName} 
-            outerStyle={[styles.outerStyle]} 
-          /> 
-
-          <TextField 
-            label='Last Name*' 
-            valueSet={setLastName} 
-            defaultValue={lastName} 
-            outerStyle={[styles.outerStyle]} 
-          /> 
-          <TextField 
-            label='Email Address*' 
-            valueSet={setEmail} 
-            defaultValue={email} 
-            outerStyle={[styles.outerStyle]} 
-          /> 
+          <ShadowView style={styles.shadowContainer}>
+            <TextField
+              label='First Name*'
+              valueSet={setFirstName}
+              defaultValue={firstName}
+              outerStyle={[styles.outerStyle]}
+            />
+          </ShadowView>
+          <ShadowView style={styles.shadowContainer}>
+            <TextField
+              label='Last Name*'
+              valueSet={setLastName}
+              defaultValue={lastName}
+              outerStyle={[styles.outerStyle]}
+            />
+          </ShadowView>
+          <ShadowView style={styles.shadowContainer}>
+            <TextField
+              label='Email Address*'
+              valueSet={setEmail}
+              defaultValue={email}
+              outerStyle={[styles.outerStyle]}
+            />
+          </ShadowView>
           <View style={styles.dropDown}>
-            <View style={{flex: 1}}>
-              <DropDown 
+            <View style={{ flex: 1 }}>
+              <DropDown
                 label='Assign Role*'
                 defaultValue={role}
                 valueSet={setRole}
-                dataList={['Regular','Owner']} 
-                outerStyle={{marginTop:hp(3)}}
+                dataList={['Regular', 'Owner']}
+                outerStyle={{ marginTop: hp(2) }}
                 // outerStyle={[styles.outerStyle]} 
-                dropdownStyle={styles.dropdownStyle} 
+                dropdownStyle={styles.dropdownStyle}
                 // dataRowStyle={styles.dataRowStyle}
-                dataTextStyle={{padding:3}}
-                />
+                dataTextStyle={{ padding: 3 }}
+              />
             </View>
-            <TouchableOpacity onPress={()=>setInfoClick(!infoClick)} style={styles.infoButton}>
-              <Image source={infoClick?images.user.infoClick: images.user.info}/>
+            <TouchableOpacity onPress={() => setInfoClick(!infoClick)} style={styles.infoButton}>
+              <Image source={infoClick ? images.user.infoClick : images.user.info} />
             </TouchableOpacity>
-              
+
           </View>
 
-          {infoClick?
-                info()
-              :null}
-          <MultiSelectGroup 
-            label='Group Access' 
-            dataList={subUserData.group} 
-            allText='All' 
-            hideSelectedDeviceLable={true}
-            hideDeleteButton={true}
-            rowStyle={styles.rowStyle}
-            dropdownStyle={{height:hp(20)}}
-            outerStyle={{marginTop:hp(2)}}
-            valueSet={setSelectedGroup} 
-            selectedData={selectedGroup}
-            selectedItemContainerStyle={styles.selectedItemContainerStyle} 
-            selectedItemRowStyle={styles.selectedItemRowStyle}
-            deleteHandle={(item)=>setSelectedGroup(selectedGroup.filter((item1) => item1.id != item.id))}
+          {infoClick ?
+            info()
+            : null}
+          <ShadowView style={styles.shadowContainer}>
+            <MultiSelectGroup
+              label='Group Access'
+              dataList={subUserData.group}
+              allText='All'
+              hideSelectedDeviceLable={true}
+              hideDeleteButton={true}
+              rowStyle={styles.rowStyle}
+              dropdownStyle={{ height: hp(20) }}
+              valueSet={setSelectedGroup}
+              selectedData={selectedGroup}
+              selectedItemContainerStyle={styles.selectedItemContainerStyle}
+              selectedItemRowStyle={styles.selectedItemRowStyle}
+              deleteHandle={(item) => setSelectedGroup(selectedGroup.filter((item1) => item1.id != item.id))}
             />
-
-          <TouchableOpacity onPress={() => addUser()} disabled={!(firstName && lastName && email && role)} style={[styles.saveButtonConatiner,{backgroundColor:firstName && lastName && email && role ? ColorConstant.BLUE : ColorConstant.LIGHTGREY}]}>
+          </ShadowView>
+          <TouchableOpacity onPress={() => addUser()} disabled={!(firstName && lastName && email && role)} style={[styles.saveButtonConatiner, { backgroundColor: firstName && lastName && email && role ? ColorConstant.BLUE : ColorConstant.LIGHTGREY }]}>
             <Text style={styles.saveText}>Save</Text>
           </TouchableOpacity>
-          </View>
-        </ScrollView>
-      </View>
-      
-      )
-    }
+        </View>
+      </ScrollView>
+    </View>
+
+  )
+}
 
 
 
 const styles = StyleSheet.create({
 
   container: {
-    alignItems:'center',
-    backgroundColor:ColorConstant.WHITE,
-    flex:1
+    alignItems: 'center',
+    backgroundColor: ColorConstant.WHITE,
+    flex: 1
   },
   subContainer: {
-    width:'85%',
-    marginTop:hp(2),
+    width: '85%',
+    marginTop: hp(2),
     marginBottom: hp(4),
     alignSelf: 'center',
   },
-  outerStyle:{
-    elevation:4,
-    marginTop:hp(3),
-    borderBottomColor:ColorConstant.GREY,
-    borderBottomWidth:1,
-    backgroundColor:ColorConstant.WHITE,
-    borderRadius:4,
+  shadowContainer: {
+    width: '100%',
     shadowColor: ColorConstant.GREY,
     shadowOffset: {
       width: 0,
-      height: 0
+      height: 3
     },
     shadowRadius: 3,
-    shadowOpacity: 1,
+    shadowOpacity: 0.3,
+    marginTop: hp(2),
   },
-  dropDown:{
-    flexDirection:'row', 
-    marginTop:hp(0.5)
-    
+  outerStyle: {
+
+    //  borderBottomColor:ColorConstant.GREY,
+    //  borderBottomWidth:1,
+    backgroundColor: ColorConstant.WHITE,
+    borderRadius: 4
+  },
+  dropDown: {
+    flexDirection: 'row',
+    marginTop: hp(0.5)
+
   },
   dropdownStyle: {
-    position:'relative', 
-    top:hp(0.1), 
-    width:'116%', 
-    left:wp(5.5),
-    marginBottom:hp(3)
+    position: 'relative',
+    top: hp(0.1),
+    width: '116%',
+    left: wp(5.5),
+    marginBottom: hp(3)
   },
   dataRowStyle: {
-    borderBottomWidth:1,borderBottomColor:ColorConstant.GREY
+    borderBottomWidth: 1, borderBottomColor: ColorConstant.GREY
   },
-  infoContainer:{
-    backgroundColor:ColorConstant.PINK,
-    borderRadius:10,
+  infoContainer: {
+    backgroundColor: ColorConstant.PINK,
+    borderRadius: 10,
     //marginVertical:hp(1),
-    marginBottom:hp(0.1),
-    padding:hp(2)
+    marginBottom: hp(0.1),
+    padding: hp(2)
   },
   infoTitle: {
     //fontSize:hp(1.3),
-    fontSize:10,
-    fontFamily:'Nunito-Regular',
-    textAlign:'center',
-    color:ColorConstant.GREY
+    fontSize: 10,
+    fontFamily: 'Nunito-Regular',
+    textAlign: 'center',
+    color: ColorConstant.GREY
   },
   infoSubContainer: {
-    flexDirection:'row',
-    padding:6
+    flexDirection: 'row',
+    padding: 6
   },
-  infoButton:{
-    paddingHorizontal:hp(2), 
-    paddingVertical:hp(5)
+  infoButton: {
+    paddingHorizontal: hp(2),
+    paddingVertical: hp(5)
   },
   role: {
     //fontSize:FontSize.FontSize.small,
-    fontSize:12,
-    fontFamily:'Nunito-Regular',
-    color:ColorConstant.BLACK,
-    flex:0.7,
-    flexWrap:'wrap'
+    fontSize: 12,
+    fontFamily: 'Nunito-Regular',
+    color: ColorConstant.BLACK,
+    flex: 0.7,
+    flexWrap: 'wrap'
   },
-  roleSubText:{
+  roleSubText: {
     //fontSize:hp(1.3),
-    color:ColorConstant.GREY,
-    flex:1,
-    fontSize:10,
-    fontFamily:'Nunito-Regular',
+    color: ColorConstant.GREY,
+    flex: 1,
+    fontSize: 10,
+    fontFamily: 'Nunito-Regular',
   },
-  saveText :{
-    textAlign:'center',
-    color:ColorConstant.WHITE,
-    fontWeight:'bold'
+  saveText: {
+    textAlign: 'center',
+    color: ColorConstant.WHITE,
+    fontWeight: 'bold'
   },
-  saveButtonConatiner :{
-    marginTop:hp(4),
-    height:hp(6),
-    width:'85%',
-    justifyContent:'center',
-    alignSelf:'center',
-    borderRadius:5
+  saveButtonConatiner: {
+    marginTop: hp(4),
+    height: hp(6),
+    width: '85%',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    borderRadius: 5
   },
-  addButton : {
-    backgroundColor:ColorConstant.ORANGE,
-    alignItems:'center',
-    justifyContent:'center',
-    width:'100%',
-    height:hp(8),
+  addButton: {
+    backgroundColor: ColorConstant.ORANGE,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: hp(6),
   },
   selectedItemRowStyle: {
-    flexDirection:'row',
-    elevation:4,
+    flexDirection: 'row',
+    elevation: 4,
     shadowColor: ColorConstant.GREY,
     shadowOffset: {
       width: 0,
@@ -325,24 +336,24 @@ const styles = StyleSheet.create({
     },
     shadowRadius: 3,
     shadowOpacity: 1,
-    backgroundColor:ColorConstant.LIGHTPINK,
-    borderRadius:5,
-    alignItems:'center',
-    paddingHorizontal:hp(1),
+    backgroundColor: ColorConstant.LIGHTPINK,
+    borderRadius: 5,
+    alignItems: 'center',
+    paddingHorizontal: hp(1),
     //flexWrap:'wrap',
-    margin:4,
-    height:hp(4),
+    margin: 4,
+    height: hp(4),
   },
-  selectedItemContainerStyle:{
-    backgroundColor:ColorConstant.PINK,
-    borderRadius:8,
-    marginTop:hp(2),
-    elevation:0,
-    padding:hp(1)
+  selectedItemContainerStyle: {
+    backgroundColor: ColorConstant.PINK,
+    borderRadius: 8,
+    marginTop: hp(2),
+    elevation: 0,
+    padding: hp(1)
   },
   rowStyle: {
-    borderBottomColor:ColorConstant.LIGHTGREY, 
-    borderBottomWidth:1
+    borderBottomColor: ColorConstant.LIGHTGREY,
+    borderBottomWidth: 1
   }
 });
 
