02f03d3c58bf524d15ea9e14d48c5f88617faeed
Khushbu Solanki <khushbu.solanki@ekzero.com>
2021-05-13 16:55:44 +0530

    Updated UI

--- Gtrack/src/screen/LiveTracking/GeoFence/GeoFence.js
+++ Gtrack/src/screen/LiveTracking/GeoFence/GeoFence.js
@@ -1,15 +1,16 @@
 
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, SafeAreaView, StyleSheet, FlatList, RefreshControl } from 'react-native';
+import { View, Text, TouchableOpacity, SafeAreaView, StyleSheet, FlatList, RefreshControl, TextInput, Image } from 'react-native';
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import { ColorConstant } from '../../../constants/ColorConstants';
 import { FontSize } from '../../../component';
 import { translate } from '../../../../App';
+import images from '../../../constants/images'
 import { useDispatch, useSelector } from 'react-redux';
 import { getGeofenceListInfo, getLoginState, isRoleRegular } from '../../Selector';
 import { SCREEN_CONSTANTS } from '../../../constants/AppConstants';
 import * as LivetrackingActions from '../Livetracking.Action'
-import { BackIcon } from '../../../component/SvgComponent';
+import { BackIcon, NextIcon } from '../../../component/SvgComponent';
 import AppManager from '../../../constants/AppManager';
 import GeofenceList from './GeofenceList';
 import GeofenceEditDialog from '../../../component/GeofenceEditDialog';
@@ -24,6 +25,11 @@ const GeoFence = ({ navigation }) => {
     const [geofenceName, setGeofenceName] = useState("")
     const [isRefreshing, setIsRefreshing] = useState(false)
     const [selectedDevice, setSelectedDevice] = useState([]);
+    const [searchKeyword, setSearchKeyword] = useState("")
+    const [isTypeClick, setIsTypeClick] = useState(false)
+    const [dropDownPos, setDropDownPos] = useState();
+    const [isMenuClick, setIsMenuClick] = useState(0)
+    const [selectedType, setSelectedType] = useState('all')
 
     const dispatch = useDispatch()
 
@@ -128,12 +134,51 @@ const GeoFence = ({ navigation }) => {
         loadGeofenceList() 
     }
 
+    const searchHandle = (keyword) => {
+        setSearchKeyword(keyword)
+        // dispatch(LivetrackingActions.requestSearchGroup(loginData.id, keyword, onSuccess, onError))
+    }
+
+    const onTypeClick = (item,key) => {
+        (key == isMenuClick) ? setIsMenuClick(-1) : setIsMenuClick(key)
+        if(item == 'All Users') {
+            setSelectedType('all')
+        }else if(item == 'Regular') {
+            setSelectedType('regular')
+        }else{
+            setSelectedType('owner')
+        }
+    }
+
+
+    const searchBar = () => {
+        return (
+                <View style={styles.search}>
+                    <TextInput 
+                        placeholder={translate("Search_here")}
+                        style={styles.searchText}
+                        onChangeText={text => searchHandle(text) }                    
+                        value={searchKeyword}
+                    />
+                </View>
+        )
+    }
+
     return (
         <SafeAreaView style={styles.container}>
+            {searchBar()}
+            <View style={{flexDirection:'row',justifyContent:'space-between',paddingHorizontal:hp(1.5),marginTop:hp(2)}}>
             { !isRegular ?
-            <TouchableOpacity style={styles.createNewMainView} onPress={() => navigation.navigate(SCREEN_CONSTANTS.GEOFENCE_CREATE_NEW)} >
-                <Text style={styles.createNewText}>{translate("Create New")}</Text>
-            </TouchableOpacity> : null }
+                <TouchableOpacity style={styles.createNewMainView} onPress={() => navigation.navigate(SCREEN_CONSTANTS.GEOFENCE_CREATE_NEW)} >
+                    <Text style={styles.createNewText}>{translate("Create New")}</Text>
+                </TouchableOpacity> : null }
+                <TouchableOpacity style={styles.allType} onPress={()=> setIsTypeClick(!isTypeClick)}>
+                    <Text style={[styles.createNewText,{color:ColorConstant.GREY}]}>All type</Text>
+                    <NextIcon/>
+                </TouchableOpacity> 
+            </View>
+
+            <View style={styles.activeUserMainView} onLayout={({nativeEvent}) => setDropDownPos(nativeEvent.layout.y)} ></View>
 
             <FlatList
                 style={{}}
@@ -149,6 +194,18 @@ const GeoFence = ({ navigation }) => {
                 }
             />
 
+            {isTypeClick ?
+                <View style={[styles.userMenu,{position:'absolute', top:dropDownPos}]}>
+                    {geofenceType.map((item, key) =>
+                        <TouchableOpacity  key={key} onPress={()=> onTypeClick(item,key)}>
+                        <Text style={[styles.userStyle,{color: (key == isMenuClick) ? ColorConstant.ORANGE : ColorConstant.BLUE}]}>{item}</Text>
+                        {key != geofenceType.length - 1 ? <View style={styles.horizontalLine} /> : null}
+                        </TouchableOpacity>
+                    )
+                    }
+                </View>
+            : null}
+
             {renderViewDialog()} 
 
             {renderDeleteDialog()}   
@@ -156,6 +213,8 @@ const GeoFence = ({ navigation }) => {
         </SafeAreaView>
     )
 }
+
+const geofenceType = ["All", "Circle", "Polygon"]
 
 const styles = StyleSheet.create({
     headerTitle: {
@@ -170,20 +229,97 @@ const styles = StyleSheet.create({
         backgroundColor: ColorConstant.WHITE,
     },
     createNewMainView: {
-        width: '95%',
-        alignSelf: 'center',
+        width: '48%',
+        // alignSelf: 'center',
         alignItems: 'center',
         justifyContent: 'space-evenly',
+        // paddingHorizontal:hp(1.5),
         backgroundColor: ColorConstant.ORANGE,
         height: hp(5),
-        borderRadius: 14,
-        marginTop: hp(3)
+        borderRadius: 10,
+        marginVertical:hp(1)
+        // marginTop: hp(3)
+    },
+    allType : {
+        alignItems:'center',
+        borderColor:ColorConstant.GREY,
+        backgroundColor:ColorConstant.WHITE,
+        borderWidth:0.5,
+        width:'48%',
+        height:hp(5),
+        borderRadius:10,
+        alignSelf:'center',
+        flexDirection:'row',
+        justifyContent:'space-around',
+        shadowColor: ColorConstant.GREY,
+        shadowOffset: { height: 0, width: 0 },
+        shadowOpacity: 1,
+        elevation: 10,
+        shadowRadius: 3
     },
     createNewText: {
         fontSize: FontSize.FontSize.small,
         fontWeight: 'bold',
         color: '#ffffff'
-    }
+    },
+    searchText: {
+        //fontSize:FontSize.FontSize.small,
+        fontSize:14,
+        color:ColorConstant.LIGHTGREY,
+        fontFamily:'Nunito-LightItalic'
+    },
+    search: {
+        paddingHorizontal:hp(2),
+        height:hp(5),
+        marginHorizontal:hp(1.5),
+        borderRadius:12,
+        marginTop:hp(4),
+        // marginBottom:hp(2),
+        elevation:4,
+        shadowColor: ColorConstant.BLACK,
+        shadowOffset: {
+            width: 0,
+            height: 0
+        },
+        shadowRadius: 3,
+        shadowOpacity: 1,
+        backgroundColor:ColorConstant.WHITE
+    },
+    horizontalLine: {
+        borderBottomWidth: 0.5, borderBottomColor: ColorConstant.GREY, margin: hp(0.7)
+    },
+    userStyle: {
+        margin: hp(0.5),
+        //color: ColorConstant.BLUE,
+        textAlignVertical: 'center',
+        paddingLeft: hp(0.5),
+        fontSize:FontSize.FontSize.small,
+        fontFamily:'Nunito-Regular'
+    },
+    activeUserMainView: {
+        justifyContent: 'space-evenly',
+        flexDirection: 'row',
+        backgroundColor:ColorConstant.WHITE
+    },
+    userMenu: {
+        backgroundColor: ColorConstant.WHITE,
+        // padding: 5,
+        paddingVertical: hp(1.5),
+        paddingHorizontal: hp(1.5),
+        right: wp(6),
+        // width:'100%',
+        //zIndex:10,
+        borderRadius: 10,
+        width: wp(42),
+        //bottom: hp(11),
+        justifyContent: 'space-between',
+        position: 'absolute',
+        shadowColor: ColorConstant.GREY,
+        shadowOffset: { height: 0, width: 0 },
+        shadowOpacity: 1,
+        elevation: 10,
+        shadowRadius: 3
+    },
 })
 
 export default GeoFence;
