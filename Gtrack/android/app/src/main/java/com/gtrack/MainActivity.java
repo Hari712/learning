d30b187c541c357c182e1974e18531822183a2ed
deep.patel <deep.patel@ekzero.com>
2022-10-17 18:40:44 +0530

    minor change

--- Gtrack/android/app/src/main/java/com/gtrack/MainActivity.java
+++ Gtrack/android/app/src/main/java/com/gtrack/MainActivity.java
@@ -1,15 +1,13 @@
 package com.gtrack;
-import android.os.Bundle;
-
-import com.facebook.react.ReactActivity;
-
 import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
+import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this, R.style.SplashStatusBarTheme,true);
+        SplashScreen.show(this, R.style.SplashStatusBarTheme);
         super.onCreate(savedInstanceState);
     }
 
